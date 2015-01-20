6fa3cb58cac51916259df59a99055f24142a166c
jden <jason@denizac.org>
2015-01-19 21:34:34 -0800

    io.js@1.0.3

--- io.js/tools/chocolateyInstall.ps1
+++ io.js/tools/chocolateyInstall.ps1
@@ -1,6 +1,6 @@
 ﻿$packageName = 'io.js'
 $fileType = 'msi'
-$url = 'https://iojs.org/dist/v1.0.2/iojs-v1.0.2-x86.msi'
+$url = 'https://iojs.org/dist/v1.0.3/iojs-v1.0.3-x86.msi'
 $silentArgs = '/quiet'
 
 Install-ChocolateyPackage $packageName $fileType "$silentArgs" "$url"
